63f260a954629fff7444d7f251d3b52e9b5305c8
Paul Osmolovsky <paul.osmolovsky@gmail.com>
2018-04-05 16:02:11 +0300

    Поиск новых координат точки

--- lab1.py
+++ lab1.py
@@ -1,5 +1,29 @@
+from math import *
 import cv2
 import numpy as np
+
+
+def stalker(image, res):
+    (h, w) = resized.shape[:2]
+    center = (w / 2, h / 2)
+
+
+def findCoord(image, angle):
+    (h, w) = image.shape[:2]
+    center = [w / 2, h / 2]
+    pointC = [center[0] + w / 2, center[1]]
+    lineAB = lineAC = L(pointC[0], pointC[1], center[0], center[1])
+    a_lambda = (180 - angle) / 2
+    lineBC = lineAB * sin(angle) / sin(a_lambda)
+    lineBE = lineBC * sin(a_lambda) / sin(90)
+    lineEC = lineBC * sin(90 - a_lambda) / sin(90)
+    pointB = [(pointC[0] - lineEC), (pointC[1] - lineBE)]
+    return [pointB, pointC]
+
+
+def L(x1, y1, x2, y2):
+    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 
 image = cv2.imread("tram.jpg")
 # Нам надо сохранить соотношение сторон
@@ -19,21 +43,16 @@ resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 (h, w) = resized.shape[:2]
 center = (w / 2, h / 2)
 
-M = cv2.getRotationMatrix2D(center, 70, 1.0)
+angle = 70
+M = cv2.getRotationMatrix2D(center, angle, 1.0)
 rotated = cv2.warpAffine(resized, M, (w, h))
 
 rect = np.zeros(rotated.shape, rotated.dtype)
-
+res = findCoord(resized, angle)
+print(res)
 cv2.imshow("Rotated image", rotated)
 cv2.waitKey(0)
 
 # cropped = resized[30:130, 150:300]
 # cv2.imshow("Cropped image", cropped)
 # cv2.waitKey(0)
-
-
-def stalker(image, res):
-    (h, w) = resized.shape[:2]
-    center = (w / 2, h / 2)
-
-
